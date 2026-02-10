37df00d02d5ff350a870400e8f97c25ac9fb7a7e
Alexander Aguero <alexjoseaguero@hotmail.com>
2026-02-10 12:58:00 +0100

    Actualizar proyecto

---
+++ ciudades.js
@@ -0,0 +1,31 @@
+const barcelona = {
+        titulo: 'Descubre la maravillosa ciudad de Barcelona: La joya de Cataluña',
+        precio: 'Obten tan solo hoy nuestro unico paquete de viaje que saldra en tan solo 300 euros',
+        subtitulo: 'Adentra en la gran historia, cultura y belleza de esta vibrante ciudad',
+        parrafo: "Bienvenidos a Barcelona, una ciudad cautivadora que combina a la perfeccion de la riquesa cultural",
+    }
+    const roma = {
+        titulo: 'Roma: Tesoro monumental en la ciudad eterna',
+        precio: 'Obten tan solo hoy nuestro unico paquete de viaje que saldra en tan solo 350 uros',
+        subtitulo: 'Adentrate a la grandeza historica, el arte y la cultura de la capital Italiana',
+        parrafo: "Bienvenidos a Roma, una ciudad que respira historia y que te transporta a traves de los siglos",
+    }
+    const paris = {
+        titulo: 'Paris: La ciudad de la Luz y el Romance',
+        precio: 'Obten tan solo hoy nuestro unico paquete de viaje que saldra en tan solo 500 euros',
+    subtitulo: 'Explora la elegancia, la cultura y la encantadora ciudad de la moda',
+    parrafo: "Bienvenidos a Paris, una ciudad que a cautivado a visitantes de todo el mundo con su belleza",
+}
+const londres = {
+    titulo: 'Londres: Descubre la magia de la ciudad de el Tamesis',
+    precio: 'Obten tan solo hoy nuestro unico paquete de viaje que saldra en tan solo 600 euros',
+    subtitulo: 'Adentrate en la historia, la diversidad y la energia de la capital britanica',
+    parrafo: "Bienvenidos a Londres, una ciudad vibrante que fusiona su rica historia con una escena contemporanea",
+}
+
+export {
+    barcelona,
+    roma,
+    paris,
+    londres
+};
--- script.js
+++ script.js
@@ -1,13 +1,38 @@
-function agregar(valor) {
-    document.getElementById('pantalla').value += valor
-}
+import { barcelona, roma, paris, londres } from "./ciudades.js";
 
-function recetear(){
-    document.getElementById('pantalla').value = ''
-}
+//Obtener los elementos del DOM
+let enlaces = document.querySelectorAll('a')
+let tituloElemento = document.getElementById('titulo')
+let subTituloElemento = document.getElementById('subtitulo')
+let parrafoElemento = document.getElementById('parrafo')
+let precioElemento = document.getElementById('precio')
 
-function calcular() {
-    const valorPantalla = document.getElementById('pantalla').value
-    const resultado = eval(valorPantalla)
-    document.getElementById("pantalla").value = resultado
+//Agregar un evento CLICK a cada enlce
+enlaces.forEach(function(enlace) {
+    enlace.addEventListener('click', function () {
+        //Remover la clase de activo de todos los enlaces 
+        enlaces.forEach(function (enlace) {
+            enlace.classList.remove('active');
+        });
+            //Agregar la clase de avtivo al enlace actual
+            this.classList.add('active')
+            //Obtener el contenido correspondienrte 
+            let contenido = obtenerContenido(this.textContent)
+
+            tituloElemento.innerHTML = contenido.titulo
+            subTituloElemento.innerHTML = contenido.subtitulo
+            parrafoElemento.innerHTML = contenido.parrafo
+            precioElemento.innerHTML = contenido.precio
+    });
+});
+
+//Funcion para traer la informacion correcta desde cuidades.js
+function obtenerContenido(enlace) {
+    let contenido = {
+        'Barcelona': barcelona,
+        'Roma': roma,
+        'Paris': paris,
+        'Londres': londres
+    };
+    return contenido[enlace];
 }
